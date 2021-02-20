c87d3319487f1bd4605d9456fb2c0766431a736a
Watotacho <watotacho@gmail.com>
2021-02-21 00:36:26 +0300

    mobile development

---
+++ src/tacho.kt
@@ -0,0 +1,9 @@
+fun main(){
+    var student= arrayOf("jamesWato","0741747514",40.567347,20,"true")
+    var name= "jamesWato"
+    var age  =20
+    var phoneNumber= "0741747514"
+    var citizen= "true"
+    var weight= 40.567347
+    print(student)
+}
